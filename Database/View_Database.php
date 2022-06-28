bcdfd559d457e089c7d2436ec1e066ecee4ba08c
Raju Jeelaga <rajujeelaga@gmail.com>
2022-06-28 16:11:34 +0530

    Database values Search functionality is added
    Database values Search functionality is added

--- Database/View_Database.php
+++ Database/View_Database.php
@@ -8,6 +8,58 @@ require_once('Include/DB.php');
 		<link rel="stylesheet" href="Include/style.css">
 	</head>
 	<body>
+		<div>
+			<fieldset>
+				<form class="" action="View_Database.php" method="GET">
+					<input type="text" name="Search" value="" placeholder="Search by name / id"><br><br>
+					<input type="submit" name="SearchButton" value="Search record">
+				</form>
+			</fieldset>
+		</div>
+		<?php if (isset($_GET['SearchButton'])){
+			$Search = $_GET['Search'];
+			global $ConnectingDB;
+			$sql = "SELECT * FROM emp_sheet WHERE name=:searcH OR emp_id=:searcH";
+			$stmt = $ConnectingDB->prepare($sql);
+			$stmt->bindValue(':searcH', $Search);
+			$stmt->execute(); ?>
+			
+
+				<table width="1000" border="5" align="center">
+					<caption>Search Result</caption>
+					<tr>
+						<th>ID</th>
+						<th>Name</th>
+						<th>EMP_ID</th>
+						<th>Desig</th>
+						<th>Salary</th>
+						<th>Address</th>
+						<th>Search Again</th>
+					</tr>
+					<?php 
+						while ( $DataRows = $stmt->fetch()){
+						$ID 		= $DataRows["id"];
+						$Name 		= $DataRows["name"];
+						$EMP_ID 	= $DataRows["emp_id"];
+						$Desig 		= $DataRows["designation"];
+						$Salary 	= $DataRows["salary"];
+						$Address 	= $DataRows["address"]; 
+					?>
+						<tr>
+							<td><?php echo $ID; ?></td>
+							<td><?php echo $Name; ?></td>
+							<td><?php echo $EMP_ID; ?></td>
+							<td><?php echo $Desig; ?></td>
+							<td><?php echo $Salary; ?></td>
+							<td><?php echo $Address;?></td>
+							<td> <a href="View_Database.php">Search Again</a> </td>
+						</tr>
+					<?php } ?>
+				</table>
+				</div>
+
+		<?php //} 
+	} ?>
 		<table width="1000" border="5" align="center">
 			<caption>View From Database</caption>
 			<tr>
@@ -44,5 +96,6 @@ require_once('Include/DB.php');
 				</tr>
 				<?php } ?>
 		</table>
+
 	</body>
 </html>
